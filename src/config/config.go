c3c319c6617dddad3d0b2d29e51402faf2274a9a
liangyandong <yandong10086@sina.com>
2019-01-25 16:19:50 +0800

    环境标签日志打印

--- src/config/config.go
+++ src/config/config.go
@@ -34,20 +34,20 @@ func init() {
 	//函数导出信息
 	funcConf()
 
-	Log.Printf("goblog starup successful appPath:%v", beego.AppPath)
+	Log.Printf("goblog starup successful env:%v,appPath:%v", beego.AppConfig.String("runmode"), beego.AppPath)
 }
 
 func dbConf() {
-	cfg := beego.AppConfig
+
 	DB = new(dbConfig)
-	DB.DbUser = cfg.DefaultString("mysqlUser", "root")
-	DB.DbPwd = cfg.DefaultString("mysqlPass", "lyd")
-	DB.DbUrls = cfg.DefaultString("mysqlUrls", "localhost:3306")
-	DB.DbName = cfg.DefaultString("mysqlDb", "goblog")
-	DB.DbMaxIdleConns = cfg.DefaultInt("mysqlMaxIdleConns", 10)
-	DB.DbMaxConns = cfg.DefaultInt("mysqlMaxOpenConns", 50)
-	DB.DbForce = cfg.DefaultBool("mysqlForce", false)
-	DB.DbDebug = cfg.DefaultBool("mysqlDebug", true)
+	DB.DbUser = beego.AppConfig.DefaultString("mysqlUser", "root")
+	DB.DbPwd = beego.AppConfig.DefaultString("mysqlPass", "lyd")
+	DB.DbUrls = beego.AppConfig.DefaultString("mysqlUrls", "localhost:3306")
+	DB.DbName = beego.AppConfig.DefaultString("mysqlDb", "goblog")
+	DB.DbMaxIdleConns = beego.AppConfig.DefaultInt("mysqlMaxIdleConns", 10)
+	DB.DbMaxConns = beego.AppConfig.DefaultInt("mysqlMaxOpenConns", 50)
+	DB.DbForce = beego.AppConfig.DefaultBool("mysqlForce", false)
+	DB.DbDebug = beego.AppConfig.DefaultBool("mysqlDebug", true)
 	orm.Debug = true
 	orm.NewLog(GetLogsWriter())
 
